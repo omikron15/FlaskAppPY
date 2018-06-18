0f4a0661bd5758ba975f4a40053b7f6730df1633
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 21:35:50 +0100

    Modifies / route to render to home.html file. home.html file created and added to new templates folder

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "INDEX2"
+    return render_template("home.html")
 
 # debug = True paramter should be removed once app is complete
 # this is only included so that server restarts are not required after every change
