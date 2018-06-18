08297ed7af869e862c1a6762f2376a8fe2be4931
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 22:42:44 +0100

    adds data.py file with seed articles data

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "author": "Author One",
+            "create_date": "01/01/2018",
+            "body": "Text text text text text"
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "author": "Author Two",
+            "create_date": "01/01/2018",
+            "body": "Text text text text text"
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "author": "Author Three",
+            "create_date": "01/01/2018",
+            "body": "Text text text text text"
+        }
+    ]
+    return articles
