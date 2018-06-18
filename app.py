5dd8613d728a3810feac15900d4806b2b664c506
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 22:37:59 +0100

    adds /about route to app.py file. Also adds about.html with content related to project

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 # debug = True paramter should be removed once app is complete
 # this is only included so that server restarts are not required after every change
 if __name__ == "__main__":
