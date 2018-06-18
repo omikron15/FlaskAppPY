e1da395d51a50fa9da57c274c7742b0089bb1252
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 22:54:00 +0100

    Adds /article/id route to app.py file

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ def about():
 def articles():
     return render_template("articles.html", articles = Articles)
 
+@app.route("/article/<string:id>/")
+def article(id):
+    return render_template("article.html", id=id)
+
 # debug = True paramter should be removed once app is complete
 # this is only included so that server restarts are not required after every change
 if __name__ == "__main__":
