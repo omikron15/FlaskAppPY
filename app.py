6228fb00be8982929ec6f374b9c7600a2afd266c
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 22:45:43 +0100

    Adds /artiles route to app.py file. Also imports articles seed data and passes ths to articles route

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route("/")
 def index():
@@ -9,6 +12,10 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles = Articles)
 
 # debug = True paramter should be removed once app is complete
 # this is only included so that server restarts are not required after every change
