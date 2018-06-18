8057e1d1f180999b27c89c27ef29b0757e108385
omikron15 <omikron15@hotmail.co.uk>
2018-06-18 21:26:23 +0100

    updated app to run in debug mode so that server restarts are not required after every change

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "INDEX"
+    return "INDEX2"
 
-
-
-
-
-
+# debug = True paramter should be removed once app is complete
+# this is only included so that server restarts are not required after every change
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
